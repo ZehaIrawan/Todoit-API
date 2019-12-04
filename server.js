a2b98b6d0d4e6a6fb6ea6c75939956a6d8823592
Zeha Irawan <jangkaringbumi@gmail.com>
2019-12-04 14:34:15 +0700

    add getTodo

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
-const express = require('express')
+const express = require('express');
+const { pool } = require('./config');
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('API Running')) 
+app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000
+const getTodos = (request, response) => {
+  pool.query('SELECT * FROM todos', (error, results) => {
+    if (error) {
+    console.log(error);
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const addTodo = (request, response) => {
+  const { title, date } = request.body;
+
+  pool.query(
+    'INSERT INTO todos (title, date) VALUES ($1, $2)',
+    [title, date],
+    error => {
+      if (error) {
+        console.log(error);
+      }
+      response.status(201).json({ status: 'success', message: 'Todo added.' });
+    },
+  );
+};
+
+app
+  .route('/todos')
+  // GET endpoint
+  .get(getTodos)
+  // POST endpoint
+  .post(addTodo);
+
+// Start server
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server listening`);
+});
